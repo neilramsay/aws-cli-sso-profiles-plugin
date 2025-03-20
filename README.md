a3a649c6fba670a247894e9ca97e07f0478d63d3
Neil Ramsay <2934552+neilramsay@users.noreply.github.com>
2025-03-20 13:12:14 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# AWS CLI SSO Profiles Plugin
+
+Extend [AWS CLI] to introduce `aws configure sso-profiles` command, which
+generates AWS CLI Profiles from the AWS Accounts and Roles available in AWS SSO.
+
+> [!WARNING]
+> This uses the "experimental" AWS CLI Plugin system, which can cause the AWS CLI
+> to stop working. The [Troubleshooting](#troubleshooting) section describes how to
+> disable this plugin.
+
+## Installation
+
+### AWS CLI default installation
+
+You need an AWS CLI version above 2.22.13 (released December 9th 2025).
+
+Upgrade if needed (see [AWS CLI install guide]):
+
+```shell
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" &&
+unzip awscliv2.zip &&
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+```
+
+We need to install the Python package somewhere which is independent
+of the AWS CLI version.
+One way is to create a Python Virtual Environment for storing plugins.
+
+```shell
+sudo python3 -m venv /usr/local/aws-cli/plugins &&
+sudo /usr/local/aws-cli/plugins/bin/pip install git+https://github.com/neilramsay/aws-cli-sso-profiles-plugin.git
+```
+
+AWS CLI needs to be configured to load the plugin on start.
+
+```shell
+aws configure set plugins.cli_legacy_plugin_path "$(realpath /usr/local/aws-cli/plugins/lib/*/site-packages/)"
+aws configure set plugins.sso_profile aws_cli_sso_profiles_plugin.sso_profiles
+```
+
+## Usage
+
+We need to configure an AWS SSO Session (if you haven't done it already).
+The following will prompt you for your AWS SSO Instance settings (SSO Start URL, and SSO Region).
+
+```shell
+aws configure sso-session
+```
+
+Once you've got an AWS SSO Session configured, you can use this plugin to
+generate AWS CLI Profiles.
+These will be written to your `~/.aws/config` file.
+
+```shell
+aws configure sso-profiles
+```
+
+## Troubleshooting
+
+### `The virtual environment was not created successfully because ensurepip is not available`
+
+```plain
+The virtual environment was not created successfully because ensurepip is not
+available.  On Debian/Ubuntu systems, you need to install the python3-venv
+package using the following command.
+
+    apt install python3.12-venv
+
+You may need to use sudo with that command.  After installing the python3-venv
+package, recreate your virtual environment.
+```
+
+On Ubuntu, Python Virtual Environments are not available by default.
+Please install it following the directions in the message (i.e. `apt install python3.12-venv` in this case.)
+
+### `No module named 'aws_cli_sso_profiles_plugin'`
+
+This happens when this plugin is not in the Python Path for aws-cli.
+
+To get going **without this plugin**, remove the `cli_legacy_plugin_path` line from your User Configuration (usually `~/.aws/config`).
+
+Otherwise, check the path in `cli_legacy_plugin_path` to ensure it points to
+a real directory, and that directory has a `aws_cli_sso_profiles_plugin` subdirectory.
+
+- If the directory is missing, create the Python virtual environment
+  (see Installation section)
+- If the directory exists, but `aws_cli_sso_profiles_plugin` is missing,
+  use the Python virtual environment `pip` (in `[directory]/bin/pip`)
+  to install this plugin (see [Installation](#installation) section)
+
+## References
+
+- [AWS CLI GitHub]
+- [AWS CLI Docs]
+
+[AWS CLI]: https://github.com/aws/aws-cli
+[AWS CLI GitHub]: https://github.com/aws/aws-cli
+[AWS CLI Docs]: https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-welcome.html
+[AWS CLI install guide]: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
---
+++ src/aws_cli_sso_profiles_plugin/sso_profiles.py
@@ -0,0 +1,257 @@
+from typing import TYPE_CHECKING
+
+from awscli.customizations.configure.sso import (
+    ConfigureSSOCommand,
+    profile_to_section,
+)
+from awscli.customizations.sso.utils import (
+    LOGIN_ARGS,
+    PrintOnlyHandler,
+    do_sso_login,
+)
+from awscli.customizations.utils import uni_print
+from botocore import UNSIGNED
+from botocore.config import Config
+from prompt_toolkit.validation import ValidationError, Validator
+
+if TYPE_CHECKING:
+    from argparse import Namespace
+    from typing import Literal, NoReturn
+
+    from awscli.commands import CLICommand
+    from botocore.hooks import HierarchicalEmitter
+    from botocore.session import Session
+    from prompt_toolkit.document import Document
+
+
+def awscli_initialize(event_hooks: "HierarchicalEmitter") -> None:
+    """Part of the Botocore Hooks Interface used by AWS CLI
+
+    Parameters
+    ----------
+    event_hooks : HierarchicalEmitter
+        Botocore hooks
+    """
+
+    # Inject our handler for `aws configure`
+    event_hooks.register(
+        event_name="building-command-table.configure", handler=_inject_commands
+    )
+
+
+def _inject_commands(
+    command_table: dict[str, "CLICommand"],
+    session: "Session",
+    **kwargs,  # noqa: ANN003
+) -> None:
+    """Inject our commands in to `aws configure`
+
+    Parameters
+    ----------
+    command_table : _type_
+        _description_
+    session : _type_
+        _description_
+    """
+    command_table["sso-profiles"] = ConfigureSSOProfiles(session)
+
+
+class ConfigureSSOProfiles(ConfigureSSOCommand):
+    NAME = "sso-profiles"
+    SYNOPSIS = "aws configure sso-profiles"
+    DESCRIPTION = (
+        "Generate AWS CLI Profiles from AWS SSO Session Accounts and Roles. \n\n"
+        "This will interactively prompt the user for the SSO Session (previously "
+        "created with `aws configure sso` or `aws configure sso-session`) and "
+        "default CLI Region for the Profiles. "
+        "These can also be supplied with the --sso-session and --region options.\n\n"
+        "Profiles will be generated with the name "
+        "[SSO_SESSION]_[AWS_ACCOUNT_NAME]_[SSO_ROLE_NAME] (with Account and Role name "
+        "spaces and '.' replaced with '-'). \n\n"
+        "The Profiles are written to the User's Configuration File "
+        "(the default location is ~/.aws/config)."
+    )
+    ARG_TABLE = LOGIN_ARGS + [
+        # CustomArgument - check out argparse
+        {
+            "name": "sso-session",
+            "help_text": "SSO Session name to lookup AWS Accounts and Roles.",
+        }
+    ]
+
+    def _run_main(
+        self, parsed_args: "Namespace", parsed_globals: "Namespace"
+    ) -> "Literal[0]":
+        # Borrowed from aws-cli ConfigureSSOCommand._run_main
+        # ---------------------8<----------------------------
+        self._unset_session_profile()
+        on_pending_authorization = None
+        if parsed_args.no_browser:
+            on_pending_authorization = PrintOnlyHandler()
+        # ---------------------------------------------------
+
+        # Choose AWS SSO Session from User Configuration File
+        sso_session_name = self._get_sso_session(parsed_args)
+        sso_session_config = self._get_sso_session_config(sso_session_name)
+
+        # Choose CLI default region for profiles written to User Configuration File
+        cli_region = self._get_cli_region(parsed_globals, sso_session_config)
+
+        # Authenticate against SSO so we can query Accounts and Roles
+        sso_token = do_sso_login(
+            session=self._session,
+            session_name=sso_session_name,
+            sso_region=sso_session_config["sso_region"],
+            start_url=sso_session_config["sso_start_url"],
+            registration_scopes=sso_session_config["registration_scopes"],
+            on_pending_authorization=on_pending_authorization,
+            parsed_globals=parsed_globals,
+        )
+
+        # Borrowed from aws-cli ConfigureSSOCommand._run_main
+        # ---------------------8<----------------------------
+        client_config = Config(
+            signature_version=UNSIGNED,
+            region_name=sso_session_config["sso_region"],
+        )
+        sso_client = self._session.create_client("sso", config=client_config)
+        # ---------------------------------------------------
+
+        # Loop through Accounts and Roles to add to User Configuration File
+        for account in self._get_all_accounts(sso_client, sso_token)["accountList"]:
+            for role in self._get_all_roles(
+                sso_client, sso_token, account["accountId"]
+            )["roleList"]:
+                self._upsert_profile(
+                    sso_session_name,
+                    account["accountName"],
+                    account["accountId"],
+                    role["roleName"],
+                    cli_region,
+                )
+
+        return 0
+
+    def _get_sso_session(self, parsed_args: "Namespace") -> str:
+        """Get the AWS SSO Session to use.
+
+        Parameters
+        ----------
+        parsed_args : Namespace
+            AWS CLI arguments
+
+        Returns
+        -------
+        str
+            AWS SSO Session name from User Configuration File
+        """
+        if parsed_args.sso_session in self._sso_sessions:
+            return parsed_args.sso_session
+
+        return self._prompter.get_value(
+            prompt_text="SSO session name",
+            current_value=None,
+            completions=sorted(self._sso_sessions),
+            validator=ValueInListValidator(
+                self._sso_sessions, None, "Not a valid SSO Session"
+            ),
+        )
+
+    def _get_cli_region(
+        self, parsed_globals: "Namespace", sso_session_config: dict
+    ) -> str:
+        """Get the AWS Region for the Profile default Region.
+
+        Parameters
+        ----------
+        parsed_globals : Namespace
+            AWS CLI Global global arguments
+        sso_session_config : _type_
+            AWS SSO Session from User Configuration File
+
+        Returns
+        -------
+        str
+            AWS Region Code (ap-southeast-2 for example)
+        """
+        # Check STS for available Regions
+        regions = self._session.get_available_regions("sts")
+
+        # Explicitly passed - use that
+        if parsed_globals.region in regions:
+            return parsed_globals.region
+
+        # Use SSO Region as default for prompt
+        default_region = sso_session_config["sso_region"]
+
+        return self._prompter.get_value(
+            prompt_text="Default client Region",
+            current_value=default_region,
+            completions=regions,
+            validator=ValueInListValidator(
+                regions, default_region, "Not a valid Region"
+            ),
+        )
+
+    def _upsert_profile(
+        self,
+        sso_session: str,
+        account_name: str,
+        account_id: str,
+        role_name: str,
+        cli_region: str,
+    ) -> None:
+        """Update Profile in User Configuration File.
+
+        Parameters
+        ----------
+        sso_session : str
+            The AWS SSO Session name
+        account_name : str
+            The AWS Account Name
+        account_id : str
+            The AWS Account ID
+        role_name : str
+            SSO IAM Role Name
+        cli_region : str
+            Default CLI Region
+        """
+        profile_name = (f"{sso_session}_{account_name}_{role_name}").translate(
+            str.maketrans({" ": "-", ".": "-"})
+        )
+        profile_values = {
+            "sso_session": sso_session,
+            "sso_account_id": account_id,
+            "sso_role_name": role_name,
+            "region": cli_region,
+        }
+
+        # Write out to console profile being written
+        uni_print(f"{profile_name}\n")
+
+        profile_section = profile_to_section(profile_name)
+        self._update_section(profile_section, profile_values)
+        self._write_new_config(profile_name)
+
+
+class ValueInListValidator(Validator):
+    """Validate provided value is a List."""
+
+    def __init__(
+        self, valid_values: list[str], default_value: str | None, error_message: str
+    ) -> None:
+        super().__init__()
+        self._valid_values = valid_values
+        self._default_value = default_value
+        self._error_message = error_message
+
+    def _raise_validation_error(self, document: "Document", message: str) -> "NoReturn":
+        index = len(document.text)
+        raise ValidationError(index, message)
+
+    def validate(self, document: "Document") -> None:
+        if document.text in self._valid_values:
+            return
+        elif document.text == "" and self._default_value in self._valid_values:
+            return
+        self._raise_validation_error(document, self._error_message)
